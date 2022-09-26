ed6a03d29beaa14b5db6f5860b1bfd40ad4ca9bd
cj <838515894@qq.com>
2022-09-26 22:53:25 +0800

    hot-fix commit1

--- src/main/java/com/git.java
+++ src/main/java/com/git.java
@@ -6,7 +6,6 @@ public class git {
         System.out.println("hello.git");
         System.out.println("hello.git");
         System.out.println("hot-fix");
-
         System.out.println("hello.git");
         System.out.println("master");
     }
